46785fde0ad6757fffab56774ae96b25a828e48a
Ducle Jaramillo <djaramillo@LABA.PSWAS.LOCAL>
2023-08-17 17:40:18 -0700

    add questions

---
+++ index.js
@@ -0,0 +1,97 @@
+// 1.
+const fruits = ["apple", "banana", "orange", "grape"]
+fruits[2] = "kiwi";
+console.log (fruits)
+
+// 2.
+const numbers= [2,4,6,8,10]
+numbers.push [12]
+console.log(numbers)
+numbers.shift();
+console.log(numbers)
+
+// 3. (need help on making it shorter)
+firstNames = ["John","Emma", "Michael"]
+lastNames = ["Doe", "Smith", "Johnson"]
+
+fullNames = [firstNames + lastNames]
+console.log(fullNames)
+ 
+fullNames = [ firstNames[0]+" "+ lastNames[0], firstNames[1]+" "+ lastNames[1], firstNames[2]+" "+ lastNames[2]];
+console.log(fullNames)
+
+// 4.
+
+ages=[25,30,18,42,15]
+ages.forEach(function()){
+  console.log (age + i)
+}
+// 5.
+scores=[85,92,78,95,88]
+newscores= scores.forEach( i <90 )
+
+// 6.
+countires=["USA","Canada","Mexico","France", "Germany"]
+if "Mexico"
+
+// 7.
+numbers=[3,7,1,9,4]
+
+// 8. grades= [87,95,76,88,92]
+
+// 9.
+data = [10,20,30,40,50]
+const newdata( i*2)
+
+
+//10. values =[34,12,78,53,90]
+
+let movie={
+  name:"Titanic",
+  releaseYear: 1997,
+  director: "James Cameron",
+  actors: ["Leonardo Dicaprio", "Kate Winslet", "Billy Zane", "Kathy Bates"]
+  starActor{
+    name: "Leonardo Dicaprio",
+    age: 5,
+    born: 1889,
+    linkToAwards: "https://en.wikipedia.org/wiki/List_of_awards_and_nominations_received_by_Leonardo_DiCaprio",
+    headshotLink: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Leonardo_DiCaprio_2014.jpg/220px-Leonardo_DiCaprio_2014.jpg"
+
+  },
+
+  budgetInMillions: 200
+
+}
+
+
+let cohortFour = {
+
+  classSize: 5,
+
+  instructor: {
+
+      name: "Fred",
+
+      age: 40,
+
+      completedCohortIds: [3, 77, 45, 23],
+
+      email: "fred@fred.com",
+
+      assistant: {
+
+          name: "Brad",
+
+          age: 38,
+
+          email: "Brad@brad.com"
+
+      }
+
+  },
+
+  classGrades: [99, 100, 89, 88, 95]
+
+}
+
